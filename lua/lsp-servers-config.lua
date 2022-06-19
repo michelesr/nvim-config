b023b453a626d50738246e5ec51130f2569585fd
Michele Sorcinelli <michelesr@autistici.org>
2022-06-19 18:25:10 +0100

    Setup json schema examples
    To be improved

--- lua/lsp-servers-config.lua
+++ lua/lsp-servers-config.lua
@@ -1,10 +1,32 @@
 local lspconfig = require('lspconfig')
 
+lspconfig['jsonls'].setup{
+  settings = {
+    json = {
+      -- type gf to open the url and look at the scheme catalog:
+      --   https://www.schemastore.org/api/json/catalog.json
+      schemas = {
+        {
+          fileMatch = {
+            '*cf.json',
+            '*cloudformation.json',
+          },
+          url = 'https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json'
+        },
+      }
+    }
+  }
+}
+
 lspconfig['yamlls'].setup{
   settings = {
     yaml = {
       schemas = {
-        ['kubernetes'] = 'foo.yaml'
+        ['kubernetes'] = 'foo.yaml',
+        ['https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json'] = {
+          '*cf.yaml',
+          '*cloudformation.yaml',
+        }
       }
     }
   }
