63723cb5f95bdd6cc2c3073dc5ff4ee72a396876
Michele Sorcinelli <michelesr@autistici.org>
2022-07-06 19:49:13 +0100

    Add some parser

--- lua/tresitter-config.lua
+++ lua/tresitter-config.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = {"python"},
+  ensure_installed = {"python", "yaml", "json", "c", "lua", "ruby"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
