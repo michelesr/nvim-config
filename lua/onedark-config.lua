4dc91bdef87cbb197e898a310bc172f833b35626
Michele Sorcinelli <michelesr@autistici.org>
2022-07-07 01:50:03 +0100

    Use dark grey as vsplit separator

--- lua/onedark-config.lua
+++ lua/onedark-config.lua
@@ -11,8 +11,8 @@ require('onedark').setup {
   -- bundle/onedark.nvim/lua/onedark/highlights.lua
   highlights = {
     VertSplit = { fg = '#8ffc67' },
-    StatusLine = { bg = '#31353f' },
-    StatusLineNC = { bg = '#31353f' },
+    StatusLine = { bg = '#222222' },
+    StatusLineNC = { bg = '#222222' },
   }
 }
 require('onedark').load()
