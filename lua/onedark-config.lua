728ffeeba483e238626f67fab986da1b7527428f
Michele Sorcinelli <michelesr@autistici.org>
2022-07-07 01:39:49 +0100

    Color overrides for onedark.nvim

--- lua/onedark-config.lua
+++ lua/onedark-config.lua
@@ -5,9 +5,14 @@ require('onedark').setup {
   colors = {
     bg0 = '#1d1d2d',
     fg = '#bdcdcd',
+    grey = '#6d7d7d',
   },
 
   -- bundle/onedark.nvim/lua/onedark/highlights.lua
-  highlights = {}
+  highlights = {
+    VertSplit = { fg = '#8ffc67' },
+    StatusLine = { bg = '#31353f' },
+    StatusLineNC = { bg = '#31353f' },
+  }
 }
 require('onedark').load()
