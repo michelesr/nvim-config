fd3cc645454bbf3d1687388a754d5f6fccc28f28
Michele Sorcinelli <michelesr@autistici.org>
2022-07-07 17:02:58 +0100

    Show relative paths in filenames in lualine

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -18,7 +18,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {
-      'filename',
+      {'filename', path = 1},
       { gps.get_location, cond = gps.is_available },
     },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
@@ -28,7 +28,9 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {'filename', path = 1}
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
