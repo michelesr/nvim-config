e7eedde2e3a491bba1f345d0e507a53ff7122990
Michele Sorcinelli <michelesr@autistici.org>
2022-07-21 13:48:06 +0100

    Enable icons

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -1,12 +1,12 @@
 local gps = require("nvim-gps")
 
 gps.setup {
-  disable_icons = true,
+  disable_icons = false,
 }
 
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'auto',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
