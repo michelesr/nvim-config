5bd0de5624c821fd09df8271cc9ad4d261e82e5b
Michele Sorcinelli <michelesr@autistici.org>
2023-01-03 20:27:36 +0000

    Telescope: use <C-j> and <C-k> to move like in fzf.vim

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,6 +1,7 @@
 -- telescope-fzf-native.nvim
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
+local actions = require('telescope.actions')
 require('telescope').setup {
   extensions = {
     fzf = {
@@ -9,6 +10,14 @@ require('telescope').setup {
       override_file_sorter = true,     -- override the file sorter
       case_mode = 'smart_case',        -- or 'ignore_case' or 'respect_case'
       -- the default case_mode is 'smart_case'
+    }
+  },
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-j>'] = actions.move_selection_next
+      }
     }
   }
 }
