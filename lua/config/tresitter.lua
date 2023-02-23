b3d46b58085670590dfa048bf57b8feedad86658
Michele Sorcinelli <michelesr@autistici.org>
2023-02-23 23:24:59 +0000

    Enable tresitter highlighting

--- lua/config/tresitter.lua
+++ lua/config/tresitter.lua
@@ -16,6 +16,7 @@ require'nvim-treesitter.configs'.setup {
   -- List of parsers to ignore installing (for "all")
   ignore_install = {},
 
+  indent = {enable = true},
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
