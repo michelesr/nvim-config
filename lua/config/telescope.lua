274bb4f75d5e1dcc13bf4eabad3e99f58b73dbaf
Michele Sorcinelli <michelesr@autistici.org>
2023-02-25 01:27:03 +0000

    style: set 2 spaces for indentation

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,23 +3,23 @@
 -- the loading is important
 local actions = require('telescope.actions')
 require('telescope').setup({
-    extensions = {
-        fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = 'smart_case', -- or 'ignore_case' or 'respect_case'
-            -- the default case_mode is 'smart_case'
-        },
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = 'smart_case', -- or 'ignore_case' or 'respect_case'
+      -- the default case_mode is 'smart_case'
     },
-    defaults = {
-        mappings = {
-            i = {
-                ['<C-k>'] = actions.move_selection_previous,
-                ['<C-j>'] = actions.move_selection_next,
-            },
-        },
+  },
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-j>'] = actions.move_selection_next,
+      },
     },
+  },
 })
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
