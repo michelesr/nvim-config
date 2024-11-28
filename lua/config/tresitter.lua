144f3edfaba44bcf729d6244d94d87382d776a6b
Michele Sorcinelli <michelesr@autistici.org>
2024-11-28 17:34:02 +0000

    fix(tresitter): Add missing setup fields

--- lua/config/tresitter.lua
+++ lua/config/tresitter.lua
@@ -1,4 +1,11 @@
 require('nvim-treesitter.configs').setup({
+
+  -- Additional modules to load
+  modules = {},
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = false,
+
   -- A list of parser names, or "all"
   ensure_installed = {
     -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
@@ -27,8 +34,6 @@ require('nvim-treesitter.configs').setup({
 
   -- List of parsers to ignore installing (for "all")
   ignore_install = {},
-
-  indent = { enable = true },
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
