074cfa8b7885733ebe9f65aed0158af4aad44da9
Michele Sorcinelli <michelesr@autistici.org>
2025-01-26 15:34:26 +0000

    fix(plugins.lua) Use vim.uv instead of deprecated vim.loop

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@
 -- boostrap section: automatically install lazy.nvim package manager if not installed already --
 -- ---------------------------------------------------------------------------------------------
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
