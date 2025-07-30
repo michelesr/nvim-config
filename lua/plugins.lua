9513dd77cbc61ae47f76825dc34df87d744e295e
Michele Sorcinelli <michelesr@autistici.org>
2025-07-31 00:59:26 +0100

    Add render markdown to beautify avante chat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -172,6 +172,14 @@ require('lazy').setup({
       'MunifTanjim/nui.nvim',
       'Kaiser-Yang/blink-cmp-avante',
       {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'Avante' },
+        },
+        ft = { 'Avante' },
+      },
+      {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
         opts = {},
