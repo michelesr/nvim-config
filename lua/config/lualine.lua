1096dc95306d0d07f2a44ab28e83648641d0cee5
Michele Sorcinelli <michelesr@autistici.org>
2026-06-09 18:09:04 +0100

    Minuet: add lualine widget
    To show LLM processing

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -19,7 +19,7 @@ return {
     lualine_c = {
       { 'filename', path = 1 },
     },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_x = { require('minuet.lualine'), 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
